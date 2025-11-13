5d2df6cca149a40510fc6eef1326573fa56225cd
doubleM <wmsc0202@naver.com>
2025-11-13 16:16:30 +0900

    fix: screenTrack -> screenVideoTracks로 변경

--- src/AgoraMultiMedia.jsx
+++ src/AgoraMultiMedia.jsx
@@ -288,7 +288,7 @@ const AgoraMultiMedia = () => {
       await client.publish(publishTracks);
       console.log('화면 트랙 게시 완료');
 
-      screenTrack.on("player-status-change", (evt) => {
+      publishTracks.on("player-status-change", (evt) => {
         console.log('플레이어 상태 변경:', evt);
       });
 
